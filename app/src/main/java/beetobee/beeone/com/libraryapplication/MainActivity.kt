01e6eaf9c2fa6353f199e3725d638860cdf8690b
sarath nambiar <sarath.ideoder@gmail.com>
2017-12-11 17:28:58 +0530

    initial commit

---
+++ app/src/main/java/beetobee/beeone/com/libraryapplication/MainActivity.kt
@@ -0,0 +1,39 @@
+package beetobee.beeone.com.libraryapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.annotation.IntegerRes
+import android.widget.TextView
+import beetobee.beeone.com.mylibrary.CentralTendency
+import java.util.Random
+import java.util.ArrayList
+import java.util.Collections
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var textView: TextView = findViewById<TextView>(R.id.tv) as TextView
+
+        val numbers = ArrayList<Int>()
+        val r = Random()
+
+        for (i in 0..14) {
+            numbers.add(r.nextInt(7) + 1)
+        }
+
+        val mean = CentralTendency.arithmeticMean(numbers).toDouble()
+        val median = CentralTendency.arithmeticMean(numbers)
+        val mode = CentralTendency.arithmeticMean(numbers)
+
+        Collections.sort(numbers)
+
+        val res = String.format("Numbers:\n\n%s\nMean: %.1f\nMedian: %.1f\nMode: %s\n",
+                numbers, mean, median, mode)
+
+        textView.text = res
+
+    }
+}
---
+++ mylibrary/src/main/java/beetobee/beeone/com/mylibrary/CentralTendency.java
@@ -0,0 +1,49 @@
+package beetobee.beeone.com.mylibrary;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Created by admin on 11-12-2017.
+ */
+
+// contains functions to computer Arithmetic mean, median and mode
+
+public class CentralTendency {
+    /**
+     * Computes the arithmetic mean of a set of numbers
+     *
+     * @param numbers
+     * @return
+     */
+    public static Number arithmeticMean(Collection<? extends Number> numbers) {
+        //check type of values
+        Iterator<? extends Number> iterator = numbers.iterator();
+        Number firstNumberInCollection = iterator.next();
+
+        if (firstNumberInCollection instanceof Integer) {
+            return computeMeanInt(numbers);
+        } /*else if (firstNumberInCollection instanceof Long) {
+            //return computeMeanLong(numbers);
+        }
+
+        //return computeMeanDouble(numbers);*/
+        return computeMeanInt(numbers);
+    }
+
+
+    /**
+     * computes arithmetic mean for a set of Integer values
+     *
+     * @param numbers
+     * @return
+     */
+    private static Number computeMeanInt(Collection<? extends Number> numbers) {
+        double result = 0;
+        Iterator<? extends Number> iterator = numbers.iterator();
+        while (iterator.hasNext()) {
+            result += iterator.next().intValue();
+        }
+        return result / numbers.size();
+    }
+}
