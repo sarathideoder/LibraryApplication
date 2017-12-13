0f62599149e61429a1cb3e60abc778ce3e85bda7
sarath nambiar <sarath.ideoder@gmail.com>
2017-12-13 11:00:58 +0530

    Signed-off-by: sarath nambiar <sarath.ideoder@gmail.com>

--- mylibrary/src/main/java/beetobee/beeone/com/mylibrary/SplashScreenActivity.kt
+++ mylibrary/src/main/java/beetobee/beeone/com/mylibrary/SplashScreenActivity.kt
@@ -2,11 +2,19 @@ package beetobee.beeone.com.mylibrary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_splash_screen)
+
     }
 }
